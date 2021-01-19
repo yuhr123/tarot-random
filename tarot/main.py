d41f19d8a74795894a8e549f05d1ce3d5240acde
Herald Yu <yuhr123@gmail.com>
2021-01-19 12:16:45 +0800

    允许用户设置抽牌数量

--- tarot/main.py
+++ tarot/main.py
@@ -1,5 +1,5 @@
 import random
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, url_for, redirect, flash
 from .tools import cards, shuffle_deck
 
 bp = Blueprint('main', __name__, url_prefix='')
@@ -10,8 +10,17 @@ bp = Blueprint('main', __name__, url_prefix='')
 def index():
     return render_template('index.html')
 
-@bp.route('/cards/<int:num>/', methods=['GET', 'POST'])
-def get_cards(num):
+@bp.route('/cards/', methods=['POST'])
+def get_cards():
+    if not request.form['num'].isdigit():
+        flash('注意：请输入正确的数字')
+        return redirect(url_for('main.index'))
+        
+    num = int(request.form['num'])
+    if num > 78:
+        flash('注意：抽取数量不能大于78！')
+        return redirect(url_for('main.index'))
+
     new_deck = shuffle_deck()
     no = random.sample(range(78), num)
     deck = cards()
